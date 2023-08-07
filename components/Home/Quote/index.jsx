36afd04509aa64801fadefcacf2cd3e250999f94
safemedigo1 <emarketing.ser@gmail.com>
2023-08-08 01:34:47 +0300

    ..

--- components/Home/Quote/index.jsx
+++ components/Home/Quote/index.jsx
@@ -1,5 +1,5 @@
 import { Checkbox, Container, Typography, FormControlLabel, Button } from '@mui/material'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import imgs from '../../../assets/constants/imgs';
 import styles from './index.module.scss';
 import Image from 'next/image';
@@ -8,17 +8,121 @@ import { motion } from "framer-motion";
 
 const Quote = () => {
   const { quote } = imgs;
-  const [step, setStep] = useState(2);
+  const [step, setStep] = useState(1);
+  const [phoneNum, setPhoneNum] = useState();
+  const [updatedFormData, setUpdatedFormData] = useState()
+  const [formData, setFormData] = useState({
+    fname: '',
+    lname: '',
+    email: '',
+    phone: '',
+    selectedForm: '',
+
+  });
+  const [selectedValues, setSelectedValues] = useState([]);
 
 
   const treatments = [
-    { treatment: 'Unknown problems after trying to conceive for a year or more.' },
-    { treatment: 'Repeated failed IVF/ICSI/Repeated Miscarriages/Blocked tubes' },
-    { treatment: 'Irregular periods/Polycystic Ovary Syndrome/Ovulation disorders/endocrinology problems' },
-    { treatment: 'Pelvic or Back pain/Uterine problems' },
-    { treatment: 'Very low/zero sperm count/quality (the husband)' },
-    { treatment: 'Personal factors/overage/Unexplained infertility.' },
+    { treatment: 'Myoma Treatment' },
+    { treatment: 'Endometriosis Treatment' },
+    { treatment: 'Polycystic Ovary Syndrome Treatment' },
+    { treatment: 'Menopause Treatment' },
+    { treatment: 'IVF / Fertility Treatment' },
+    { treatment: 'IUI' },
+    { treatment: 'Ovarian Stimulation' },
+    {
+      treatment: 'Egg Collection'
+    },
+    { treatment: 'Micro TESE' },
+    { treatment: 'Embryo Transfer    ' },
+    { treatment: 'Genetic Diagnosis    ' },
+    {
+      treatment: 'Check Up    '
+    },
+    { treatment: 'Pregnancy Care    ' },
+    { treatment: 'Childbirth' },
+    { treatment: 'Aesthetic treatments    ' },
+    { treatment: 'Others' },
   ]
+
+
+
+
+
+
+  const handleChangeFrom = (event) => {
+    const { name, value, checked } = event.target;
+
+    setFormData({
+      ...formData,
+      [name]: name === 'agree' ? checked : value,
+    });
+
+
+  };
+
+  const handleChangePhone = (newPhoneVal, countryData) => {
+    setPhoneNum(newPhoneVal);
+  };
+
+
+  useEffect(() => {
+    setUpdatedFormData({ ...formData, phone: phoneNum, selectedForm: selectedValues, })
+
+
+  }, [formData]);
+
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    // Submit the form data to the server
+    // Add the phone number to the form data
+    // const updatedFormData = { ...formData, phone: phoneNum };
+
+
+
+
+
+    if (updatedFormData) {
+      const url = `/api/sendEmail`;
+      const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ ...updatedFormData }),
+      });
+
+      if (response) {
+        // setIsLoading(false)
+        console.log(response)
+        console.log(updatedFormData)
+      }
+      if (response.status === 200) {
+        // handleCodeSubmit();
+        setStep(step + 1);
+
+      }
+
+
+    }
+
+  };
+
+  const handleCheckboxChange = (event) => {
+    const value = event.target.value;
+    const checked = event.target.checked;
+
+    if (checked) {
+      setSelectedValues([...selectedValues, value]);
+    } else {
+      setSelectedValues(selectedValues.filter((val) => val !== value));
+    }
+  };
+
+  console.log(updatedFormData)
+
 
   return (
     <section id='quote' className={styles.quote}>
@@ -46,50 +150,52 @@ const Quote = () => {
             <form action="">
 
               {step === 1 &&
-                <motion.div
-                  animate={{ opacity: 1 }}
-                  initial={{ opacity: 0 }}>
+                <>
+
+                  <motion.div
+                    animate={{ opacity: 1 }}
+                    initial={{ opacity: 0 }}
+                    className={styles.treatments}
+                  >
 
 
-                  {treatments.map((treatment, index) =>
+                    {treatments.map((treatment, index) =>
+                      <FormControlLabel
+                        key={index}
 
-
-                    <FormControlLabel
-                      key={index}
-
-                      sx={{
-                        marginLeft: 0
-
-                      }}
-                      required
-                      control={<Checkbox
-                        value={treatment.treatment}
-                        // checked={selectedValues.includes(treatment.treatment)}
-                        // onChange={handleCheckboxChange}
                         sx={{
-                          color: '#1B0968',
-                          '.Mui-checked': {
-                            color: '#1B0968 ',
-                          },
-                          '.MuiCheckbox-colorSecondary.Mui-checked': {
-                            color: '#1B0968 ',
-                          },
-                          '.MuiIconButton-root': {
-                            color: '#1B0968 ',
-                          },
-                          marginLeft: 0
+                          marginLeft: 0,
 
-                        }} />} label={treatment.treatment} />
-                  )}
+                        }}
+                        required
+                        control={<Checkbox
+                          value={treatment.treatment}
+                          checked={selectedValues.includes(treatment.treatment)}
+                          onChange={handleCheckboxChange}
+                          sx={{
+                            color: '#1B0968',
+                            '.Mui-checked': {
+                              color: '#1B0968 ',
+                            },
+                            '.MuiCheckbox-colorSecondary.Mui-checked': {
+                              color: '#1B0968 ',
+                            },
+                            '.MuiIconButton-root': {
+                              color: '#1B0968 ',
+                            },
+                            marginLeft: 0
 
+                          }} />} label={treatment.treatment} />
+                    )}
+
+
+                  </motion.div>
                   <div className={styles.nextBtn} onClick={() => setStep(2)}>
                     <Button>
                       Next
                     </Button>
                   </div>
-
-                </motion.div>
-
+                </>
               }
 
 
@@ -102,15 +208,15 @@ const Quote = () => {
                   className={styles.input_container}>
 
                   <div className={styles.name}>
-                    <input type="text" placeholder='Your First Name' />
-                    <input type="text" placeholder='Your Second Name' />
+                    <input type="text" placeholder='Your First Name' name='fname' onChange={handleChangeFrom} required />
+                    <input type="text" placeholder='Your Second Name' name='lname' onChange={handleChangeFrom} required />
                   </div>
 
                   <div className={styles.phone}>
                     <PhoneInput
                       country={'tr'}
-                      value=''
-                      // onChange={handleChangePhone}
+                      value={phoneNum}
+                      onChange={handleChangePhone}
                       // onChange={newPhoneVal => setPhoneNum(newPhoneVal)}
 
                       inputProps={{
@@ -121,13 +227,13 @@ const Quote = () => {
                   </div>
 
                   <div className={styles.email}>
-                    <input type="email" placeholder='E-mail' />
+                    <input type="email" placeholder='E-mail' name='email' onChange={handleChangeFrom} required />
                   </div>
 
 
-                  <div className={styles.nextBtn} onClick={() => setStep(1)}>
+                  <div className={styles.nextBtn} onClick={handleSubmit}>
                     <Button>
-                      Next
+                      Submit
                     </Button>
                   </div>
                 </motion.div>
@@ -136,6 +242,24 @@ const Quote = () => {
 
 
             </form>
+
+            {step === 3 &&
+              <motion.div
+                animate={{ opacity: 1 }}
+                initial={{ opacity: 0 }}
+                className={styles.conf_msg}
+              >
+                <div className={styles.title}>
+                  <Typography variant='h5'>We Got Your Request</Typography>
+                </div>
+
+                <div className={styles.desc}>
+                  <Typography>Thank You For Your Submission. Our Team Will Evaluate Your Request And Respond To You In A Timely Manner</Typography>
+                </div>
+
+              </motion.div>
+
+            }
 
 
           </div>
