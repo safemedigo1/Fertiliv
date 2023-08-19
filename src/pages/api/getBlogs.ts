4b2c37151b097e3deebfef31033cc1c3e50bdbc5
safemedigo1 <emarketing.ser@gmail.com>
2023-08-19 12:16:19 +0300

    ..

--- src/pages/api/getBlogs.ts
+++ src/pages/api/getBlogs.ts
@@ -1,130 +1,92 @@
-// import fs from "fs";
+// import { NextApiHandler } from "next";
+// import fs from "fs/promises";
 // import path from "path";
+// import Cors from "cors";
 
-// export default function getBlogs(req, res) {
-//   if (req.method === "GET") {
-//     // Path to the blogs.json file in the "./" directory
-//     const filePath = path.join(process.cwd(), "blogs.json");
+// const isDev = process.env.NODE_ENV === "development";
+// const baseUrl = isDev ? "http://localhost:3000" : "https://fertilive.com";
 
-//     try {
-//       // // Read the contents of the blogs.json file
-//       // const jsonData = fs.readFileSync(filePath, "utf-8");
-//       // // Parse the JSON data into an array of blogs
-//       // const blogs = JSON.parse(jsonData);
+// // Helper function to wrap the cors middleware
+// const cors = (handler: NextApiHandler) => async (req, res) => {
+//   await new Promise((resolve, reject) => {
+//     Cors()(req, res, (result: unknown) => {
+//       if (result instanceof Error) {
+//         return reject(result);
+//       }
+//       return resolve(result);
+//     });
+//   });
 
-//       // // Example response
-//       // res.status(200).json(jsonData);
+//   return handler(req, res);
+// };
 
-//       const jsonData = fs.readFileSync(filePath, "utf-8");
-//       // Parse the JSON data into an array of blogs
-//       const blogs = JSON.parse(jsonData);
+// const getBlogs: NextApiHandler = async (req, res) => {
+//   try {
+//     // Run the cors middleware
+//     await cors(req, res);
 
-//       // Update the image URLs to include the correct host
-//       const updatedBlogs = blogs.map((blog) => {
-//         // Assuming the image property is named "image" in the JSON data
+//     const blogsFilePath = path.join(process.cwd(), "blogs.json");
+//     const data = await fs.readFile(blogsFilePath, "utf8");
+//     const blogs = JSON.parse(data);
 
-//         const imageUrl = blog.image;
-//         // Check if the image URL is a local path
+//     // Update the image URLs to use the appropriate base URL
+//     const blogsWithFixedImageUrls = blogs.map((blog) => {
+//       return {
+//         ...blog,
+//         image: `${baseUrl}${blog.image}`,
+//       };
+//     });
 
-//         if (imageUrl.startsWith("/images/")) {
-//           // Get the host from the request
-//           const host = req.headers.host;
-
-//           // Update the image URL with the correct host
-//           blog.image = `http://${host}${imageUrl}`;
-//         }
-//         return blog;
-//       });
-//       res.status(200).json(updatedBlogs);
-//       console.log(updatedBlogs);
-//     } catch (error) {
-//       console.error(error); // Log the error for debugging purposes
-//       res.status(500).json({ error: "Internal Server Error" });
-//     }
-//   } else {
-//     // Handle other HTTP methods if needed
-//     res.status(405).json({ error: "Method Not Allowed" });
+//     res.status(200).json(blogsWithFixedImageUrls);
+//   } catch (error) {
+//     console.error("Error reading blogs.json", error);
+//     res.status(500).json({ error: "Internal Server Error" });
 //   }
-// }
-// import fs from "fs";
-// import path from "path";
-// import { v4 as uuidv4 } from "uuid";
+// };
 
-// export default async function getBlogs(req, res) {
-//   if (req.method === "GET") {
-//     // Path to the blogs.json file in the "./" directory
-//     const filePath = path.join(process.cwd(), "blogs.json");
+// export default getBlogs;
 
-//     try {
-//       // Read the contents of the blogs.json file
-//       const jsonData = fs.readFileSync(filePath, "utf-8");
-//       // Parse the JSON data into an array of blogs
-//       const blogs = JSON.parse(jsonData);
-
-//       // Update the image URLs to use direct URLs to the image files
-//       const updatedBlogs = await Promise.all(
-//         blogs.map(async (blog) => {
-//           // Assuming the image property is named "image" in the JSON data
-//           const imageUrl = blog.image;
-//           // Check if the image URL is a blob URL
-//           if (imageUrl.startsWith("blob:")) {
-//             // Remove the "blob:" prefix from the URL
-//             const blobUrl = imageUrl.substring(5);
-//             console.log(blobUrl, "HERE");
-//             // Generate a unique filename using UUID
-//             const fileName = `${uuidv4()}.png`;
-//             // Define the destination directory for the image files
-//             const destDir = path.join(process.cwd(), "public/images");
-//             // Create the destination directory if it doesn't exist
-//             fs.mkdirSync(destDir, { recursive: true });
-//             // Download the image file using fetch
-//             const response = await fetch(blobUrl);
-//             const arrayBuffer = await response.arrayBuffer();
-//             // Convert the ArrayBuffer to a Buffer
-//             const buffer = Buffer.from(arrayBuffer);
-//             // Save the image file to the destination directory
-//             fs.writeFileSync(path.join(destDir, fileName), buffer);
-//             // Update the image URL with the direct URL to the image file
-//             blog.image = `/public/images/${fileName}`; // The images folder is inside the public directory
-//           }
-//           return blog;
-//         })
-//       );
-
-//       // Example response
-//       res.status(200).json(updatedBlogs);
-//     } catch (error) {
-//       console.error(error); // Log the error for debugging purposes
-//       res.status(500).json({ error: "Internal Server Error" });
-//     }
-//   } else {
-//     // Handle other HTTP methods if needed
-//     res.status(405).json({ error: "Method Not Allowed" });
-//   }
-// }
-
-import { NextApiHandler } from "next";
+import { NextApiHandler, NextApiResponse } from "next";
 import fs from "fs/promises";
 import path from "path";
+import Cors from "cors";
 
 const isDev = process.env.NODE_ENV === "development";
 const baseUrl = isDev ? "http://localhost:3000" : "https://fertilive.com";
 
-const getBlogs: NextApiHandler = async (req, res) => {
-  try {
-    const blogsFilePath = path.join(process.cwd(), "blogs.json");
-    const data = await fs.readFile(blogsFilePath, "utf8");
-    const blogs = JSON.parse(data);
-
-    // Update the image URLs to use the appropriate base URL
-    const blogsWithFixedImageUrls = blogs.map((blog) => {
-      return {
-        ...blog,
-        image: `${baseUrl}${blog.image}`,
-      };
+// Helper function to wrap the cors middleware
+const cors =
+  (handler: NextApiHandler) => async (req: any, res: NextApiResponse) => {
+    await new Promise((resolve, reject) => {
+      Cors()(req, res, (result: unknown) => {
+        if (result instanceof Error) {
+          return reject(result);
+        }
+        return resolve(result);
+      });
     });
 
-    res.status(200).json(blogsWithFixedImageUrls);
+    return handler(req, res);
+  };
+
+const getBlogs: NextApiHandler = async (req, res) => {
+  try {
+    // Run the cors middleware
+    await cors(async (req: any, res: NextApiResponse) => {
+      const blogsFilePath = path.join(process.cwd(), "blogs.json");
+      const data = await fs.readFile(blogsFilePath, "utf8");
+      const blogs = JSON.parse(data);
+
+      // Update the image URLs to use the appropriate base URL
+      const blogsWithFixedImageUrls = blogs.map((blog: any) => {
+        return {
+          ...blog,
+          image: `${baseUrl}${blog.image}`,
+        };
+      });
+
+      res.status(200).json(blogsWithFixedImageUrls);
+    })(req, res);
   } catch (error) {
     console.error("Error reading blogs.json", error);
     res.status(500).json({ error: "Internal Server Error" });
